43536329a213dcb8cafc0859342f2eaed5b154c9
sherjilis-haxr <mohammadkhan305859ejd@gmail.com>
2026-05-30 12:26:42 -0700

    added screen

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 import pygame
-
-pygame.init()
+import network
+import ships
+import ui
 
 print("test")
 print("testtesttest")
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))  # window size
+pygame.display.set_caption("Battleship")
+
+# main game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    # draw stuff here
+    screen.fill((0, 0, 255))  # background color
+    pygame.display.flip()
+
+pygame.quit()
+
