56625746063bf680d6712d3945530ba232553b82
sherjilis-haxr <mohammadkhan305859ejd@gmail.com>
2026-05-30 12:44:01 -0700

    Co-authored-by: Ethan Trinh <pr0ph3t-lol@users.noreply.github.com>

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ import ui
 
 print("test")
 print("testtesttest")
+ships = {"Aircraft_Carrier": ships.ship("Aircraft Carrier", 5), 
+         "Battle_ship": ships.ship("Battleship", 4),
+         "Submarine": ships.ship("Submarine", 3),
+         "Destroyer": ships.ship("Destroyer", 3),
+         "Patrol_Boat": ships.ship("Patrol Boat", 2)}
+
 pygame.init()
 
 screen = pygame.display.set_mode((800, 600))  # window size
@@ -18,7 +24,9 @@ while running:
             running = False
     
     # draw stuff here
-    screen.fill((0, 0, 255))  # background color
+    grid = ui.Grid(10, 10, 50, 50)
+    grid.draw(screen)  # create a grid with 10 rows and 10 columns, each cell is 50x50 pixels
+    screen.fill((255, 0, 0))  # background color
     pygame.display.flip()
 
 pygame.quit()
